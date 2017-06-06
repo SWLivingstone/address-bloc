69bb721787aca2ccac4a72da81ed19bdc8fa0c15
SWLivingstone <scottwlivingstone@gmail.com>
2017-06-06 12:10:45 -0700

    checkpoint 4 assignment.  Add remove_entry method to AddressBook model.

--- models/address_book.rb
+++ models/address_book.rb
@@ -9,12 +9,20 @@ class AddressBook
 
   def add_entry(name, phone_number, email)
      index = 0
-     entries.each do |entry|
+     @entries.each do |entry|
        if name < entry.name
          break
        end
        index+= 1
      end
-     entries.insert(index, Entry.new(name, phone_number, email))
+     @entries.insert(index, Entry.new(name, phone_number, email))
+  end
+
+  def remove_entry(name, phone_number, email)
+    @entries.each do |entry|
+      if entry.name == name and entry.phone_number == phone_number and entry.email == email
+        @entries.delete(entry)
+      end
+    end
   end
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -29,10 +29,29 @@ RSpec.describe AddressBook do
        book = AddressBook.new
        book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
        new_entry = book.entries[0]
- 
+
        expect(new_entry.name).to eq('Ada Lovelace')
        expect(new_entry.phone_number).to eq('010.012.1815')
        expect(new_entry.email).to eq('augusta.king@lovelace.com')
      end
+  end
+
+  describe "#remove_entry" do
+
+    it "removes the correct entry from address book using name,phone_number, and email" do
+      book = AddressBook.new
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      book.add_entry('Boba Fett', '123.456.7890', 'fake_boba@hotmail.com')
+      book.add_entry('Boba Fett', '098.765.4321', 'fett_man@bounty.com')
+      expect(book.entries.size).to eq(3)
+      book.remove_entry('Boba Fett', '123.456.7890', 'fake_boba@hotmail.com')
+      expect(book.entries.size).to eq(2)
+      expect(book.entries[0].name).to eq('Ada Lovelace')
+      expect(book.entries[0].phone_number).to eq('010.012.1815')
+      expect(book.entries[0].email).to eq('augusta.king@lovelace.com')
+      expect(book.entries[1].name).to eq('Boba Fett')
+      expect(book.entries[1].phone_number).to eq('098.765.4321')
+      expect(book.entries[1].email).to eq('fett_man@bounty.com')
+    end
   end
 end
